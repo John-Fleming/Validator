8971a2d55c6d9c0a07394bae48bfef07687e08de
John Fleming <johndeclanfleming@gmail.com>
2020-08-01 10:50:14 -0500

    phone number validation

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -33,6 +33,41 @@ namespace Validator
                 Console.WriteLine("PIN is valid");
             }
 
+            // Phone number validation - must be 10 integers; spaces dashes and parentheses are ok; can't start with 555
+
+            Console.WriteLine("Please enter a 10 digit phone number:");
+            var phoneInput = Console.ReadLine();
+            var concisePhone = phoneInput.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
+
+            var digitCount = 0;
+            var invalidChar = false;
+            var firstThreeAreFives = false;
+
+            foreach (var c in concisePhone)
+            {
+                if (char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+                else
+                {
+                    invalidChar = true;
+                }
+            }
+
+            if (concisePhone[0] == '5' && concisePhone[1] == '5' && concisePhone[2] == '5')
+            {
+                firstThreeAreFives = true;
+            }
+
+            if (digitCount == 10 && !firstThreeAreFives && !invalidChar)
+            {
+                Console.WriteLine("Phone number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid phone number");
+            }
         }
     }
 }
